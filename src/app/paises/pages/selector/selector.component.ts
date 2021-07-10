3cfd02d739515e99a1be25f4fcb192f65fbc5f85
Kyxva <antonio.calic97@gmail.com>
2021-07-09 20:51:30 -0500

    selectoresApp

---
+++ src/app/paises/pages/selector/selector.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { switchMap, tap } from 'rxjs/operators';
+
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styles: [
+  ]
+})
+export class SelectorComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region  : [ '', Validators.required ],
+    pais    : [ '', Validators.required ],
+    frontera: [ '', Validators.required ]
+  });
+
+  // Llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  // fronteras: string[] = [];
+  fronteras: PaisSmall[] = [];
+
+  // UI
+  cargando: boolean = false;
+
+  constructor( private fb: FormBuilder,
+               private paisesService: PaisesService ) { }
+
+  ngOnInit(): void {
+
+    this.regiones = this.paisesService.regiones;
+
+    // Cambio de region - Primer Selector
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap( ( _ ) => {
+          this.miFormulario.get('pais')?.reset('');
+          this.cargando = true;
+         }),
+        switchMap( region => this.paisesService.getPaisesPorRegion( region ) )
+      )
+      .subscribe( paises => {
+        this.paises = paises;
+        this.cargando = false;
+      });
+
+    // Cambio de país - Segundo Selector
+    this.miFormulario.get('pais')?.valueChanges
+      .pipe(
+        tap( () => {
+          this.fronteras = [];
+          this.miFormulario.get('frontera')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap( codigo => this.paisesService.getPaisPorCodigo( codigo ) ),
+        switchMap( pais => this.paisesService.getPaisesPorCodigos( pais?.borders! ) )
+      )
+      .subscribe( paises => {
+        // this.fronteras = pais?.borders || [];
+        this.fronteras = paises;
+        this.cargando = false;
+      })
+
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value);
+  }
+
+}
